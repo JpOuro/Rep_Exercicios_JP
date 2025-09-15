deb1d63bfd1218fd44db55a61685b0189e3ce72e
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-09-15 10:44:18 -0300

    Escreva um programa que recebe um número e verifica se ele é positivo, negativo ou zero.

---
+++ 200_Ex_JS/ex_9.js
@@ -0,0 +1,10 @@
+let a = prompt("Insira um número.");
+if (a > 0) {
+    console.log("Seu número é positivo.");
+}
+else if (a < 0) {
+    console.log("Seu número é negativo.");
+}
+else {
+    console.log("Seu número é 0.");
+}
