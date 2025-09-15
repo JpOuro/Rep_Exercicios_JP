64f5ddaf2cbf2c24b94f041f7ab75c5699c3f615
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-09-15 11:21:39 -0300

    Escreva um programa que calcula o IMC (Índice de Massa Corporal) de uma pessoa e imprime uma mensagem indicando se a pessoa está abaixo do peso, com peso normal, com sobrepeso ou obesa.

--- 200_Ex_JS/ex_13.js
+++ 200_Ex_JS/ex_13.js
@@ -1,15 +1,17 @@
-let altura = parseInt(prompt("Insira sua altura."));
-let peso = parseInt(prompt("Insira seu peso."));
-let IMC = peso / (altura ** 2);
-if (IMC >= 30 && IMC < 34.9) {
-    console.log("Você sofre de obesidade.");
+let ano = parseInt(prompt("Insira o ano."));
+if (ano % 4 == 0) {
+    if (ano % 100 == 0) {
+        if (ano % 400 == 0){
+            console.log(`O ano ${ano} não é bissexto.`);
+        }
+        else {
+            console.log(`O ano ${ano} é bissexto.`);
+        }
+    }
+    else {
+        console.log(`O ano ${ano} é bissexto.`);
+    }
 }
-else if (IMC < 18.5) {
-    console.log("Você está abaixo do peso.");
-}
-else if (IMC >= 18.5 && IMC < 25) {
-    console.log("Você está com peso normal.");
-}
-else if (IMC >= 25 && IMC < 30) {
-    console.log("Você está com sobrepeso.");
+else {
+    console.log(`O ano ${ano} não é bissexto.`);
 }
