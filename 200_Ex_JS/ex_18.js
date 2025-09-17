538f59a0712acd9bc2cd0639fa929c2272c9c8c9
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-09-17 09:07:30 -0300

    Escreva um programa que verifica se uma pessoa pode dirigir com base na idade.

---
+++ 200_Ex_JS/ex_18.js
@@ -0,0 +1,7 @@
+let idade = prompt("Insira a sua idade.");
+if (idade >= 18) {
+    console.log("Você já tem idade para dirigir.");
+}
+else {
+    console.log("Você ainda não tem idade para dirigir.");
+}
