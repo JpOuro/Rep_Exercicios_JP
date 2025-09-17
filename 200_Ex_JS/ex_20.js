972c2febf36415b7c16598b6bce9d68119d8b3df
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-09-17 09:55:28 -0300

    Escreva um programa que imprime uma classificação baseada na nota de um aluno.

---
+++ 200_Ex_JS/ex_20.js
@@ -0,0 +1,10 @@
+let nota = prompt("Insira a nota do aluno.");
+if (nota <= 3) {
+    console.log("A nota desse aluno é rank 3.");
+}
+else if(nota <= 6) {
+    console.log("A nota desse aluno é rank 2.");
+}
+else if (nota <= 10) {
+    console.log("A nota desse aluno é rank 1.");
+}
