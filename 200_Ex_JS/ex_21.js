89ea7fe7ab92f61d10df8becdcb24dcbc3cd0219
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-09-19 00:28:25 -0300

    Escreva um programa que determina a estação do ano com base no mês.

---
+++ 200_Ex_JS/ex_21.js
@@ -0,0 +1,13 @@
+let a = prompt("Digite o número do mês.");
+if (a <= 3) {
+	console.log("A estação atual é Verão.");
+}
+else if (a <= 6) {
+	console.log("A estação atual é Outono.");
+}
+else if (a <= 9) {
+	console.log("A estação atual é Inverno.")
+}
+else if (a <= 12) {
+	console.log("A estação atual é Primavera.")
+}
