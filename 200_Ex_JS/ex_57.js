a20d1b9f38eba5da9595bae875df5400d3875bef
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-09-28 17:30:00 -0300

    Escreva uma função que retorne a soma de todos os números ímpares em um array de números.

---
+++ 200_Ex_JS/ex_57.js
@@ -0,0 +1,16 @@
+    function odd_sum() {
+    let b = 0;
+    let n = prompt("Insira o tamanho do array.");
+    let arr = [];
+    for (let i = 0; i < n; i++) {
+        let a = prompt("Insira os elementos do array.");
+        arr.push(a);
+    }
+    for (let i = 0; i < n; i++) {
+        if (arr[i] % 2 != 0) {
+            b += parseInt(arr[i]);
+        }
+    }
+    return b;
+}
+console.log(odd_sum());
