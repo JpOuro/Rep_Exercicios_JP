9b38aff6538f54c186dfe8a7fd155b6c85eeecdb
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-10-04 14:36:02 -0300

    Escreva um programa que determina se um número é par ou ímpar.

---
+++ 200_Ex_TS/ex_17.ts
@@ -0,0 +1,8 @@
+let a:number = parseInt(prompt("Insira um número."));
+if (a % 2 == 0) {
+    console.log(`O número ${a} é par.`);
+}
+else {
+    console.log(`O número ${a} é ímpar.`);
+}
+export{}
