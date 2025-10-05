b43cd74d6ad9fa738b8f9c14b9b20c6da08f53fa
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-10-05 11:24:37 -0300

    Escreva um programa que verifica as cores de um semáforo.

---
+++ 200_Ex_TS/ex_23.ts
@@ -0,0 +1,11 @@
+let a: string = prompt("Insira a cor do semáforo.");
+if (a == "Verde") {
+    console.log("Você deveria seguir.");
+}
+else if (a == "Amarelo") {
+    console.log("Você deveria desacelerar.");
+}
+else {
+    console.log("Você deveria parar.");
+}
+export{}
