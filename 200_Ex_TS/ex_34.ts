eb4a9d014a5d9a60951c6d7e22e107673e54b252
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-10-06 11:10:10 -0300

    Usando um laço while, imprima os números de 1 a 100, mas para múltiplos de 3 imprima Fizz e para múltiplos de 5 imprima Buzz. Para números que são múltiplos de ambos, imprima FizzBuzz (Problema FizzBuzz).

---
+++ 200_Ex_TS/ex_34.ts
@@ -0,0 +1,16 @@
+let i: number = 0;
+while (i < 100) {
+    i++;
+    if (i % 3 == 0) {
+        if (i % 5 == 0) {
+            console.log("FizzBuzz");
+        }
+        else {
+            console.log("Fizz");
+        }
+    }
+    else if (i % 5 == 0) {
+        console.log("Buzz");
+    }
+}
+export{}
