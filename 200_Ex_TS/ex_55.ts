eda781d5fb4963e6dcd87e942babca4be05604bc
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-10-11 19:36:24 -0300

    Escreva uma função que retorne o menor número em um array de números.

---
+++ 200_Ex_TS/ex_55.ts
@@ -0,0 +1,21 @@
+    function min() {
+    let n = parseInt(prompt("Insira o tamanho do array."));
+    let arr = [];
+    n++;
+    for (let i = 0; i < n; i++) {
+        let a = prompt("Insira os elementos do array.");
+        arr.push(a);
+    }
+    for (let i = 0; i < n; i++) {
+        let min = arr[i];
+        let ind;
+        for (let j = i; j < n; j++) {
+            if (min > arr[j]) {
+                min = arr[j];
+            }
+        }
+        return min;
+    }
+}
+console.log(min());
+export{}
