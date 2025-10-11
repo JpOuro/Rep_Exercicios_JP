cea2fc41c0f4472d68cd1568a619113b1fdef34a
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-10-10 23:39:34 -0300

    Escreva uma função que aceite três números como argumentos e retorne o maior deles.

---
+++ 200_Ex_TS/ex_49.ts
@@ -0,0 +1,20 @@
+function bigger(a: number, b: number, c: number) {
+    if (a >= b) {
+        if (a >= c) {
+            return a;
+        }
+        else {
+            return c;
+        }
+    }
+    else {
+        if (b >= c) {
+            return b;
+        }
+        else {
+            return c;
+        }
+    }
+}
+console.log(bigger(1,2,3));
+export{}
