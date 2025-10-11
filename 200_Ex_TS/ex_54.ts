120c29a5093e0186757490fb869b3b4731f76d60
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-10-11 19:35:42 -0300

    Escreva uma função que ordene um array de números em ordem decrescente.

---
+++ 200_Ex_TS/ex_54.ts
@@ -0,0 +1,26 @@
+    function Sort() {
+    let n: number = parseInt(prompt("Insira o tamanho do array."));
+    let arr = [];
+    n++;
+    for (let i = 0; i < n; i++) {
+        let a = prompt("Insira os elementos do array.");
+        arr.push(a);
+    }
+    for (let i = 0; i < n; i++) {
+        let max = arr[i];
+        let ind;
+        for (let j = i; j < n; j++) {
+            if (max < arr[j]) {
+                max = arr[j];
+                ind = j; 
+            }
+        }
+        arr[ind] = arr[i];
+        arr[i] = max;
+    }
+    for (let i = 0; i < n; i++) {
+        console.log(arr[i]);
+    }
+}
+Sort();
+export{}
