791eb5affcc1a8b0a304038be3529b695721fe67
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-10-11 19:33:24 -0300

    Escreva uma função que calcule o n-ésimo número de Fibonacci.

---
+++ 200_Ex_TS/ex_52.ts
@@ -0,0 +1,22 @@
+let n: number = parseInt(prompt("Insira um número."));
+function Fibonacci(n: number) {
+    let a = 0;
+    let b = 1;
+    let c;
+    if (n == 1) {
+        return a;
+    }
+    else if (n == 2) {
+        return b;
+    }
+    else {
+        for (let i = 1; i < n-1; i++) {
+            let c = a;
+            a = b;
+            b = c + b;
+        }
+        return b;
+    }
+}
+console.log(Fibonacci(n));
+export{}
