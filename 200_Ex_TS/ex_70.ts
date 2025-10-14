a4541612e5834f3ecec3d1ad33357323de8f3d29
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-10-14 12:23:44 -0300

    Escreva uma função de alta ordem que aceite um array de funções de callback e um valor, e retorne um array de resultados de aplicar cada função de callback ao valor.

---
+++ 200_Ex_TS/ex_70.ts
@@ -0,0 +1,18 @@
+let n: number = parseInt(prompt("Insira um número."));
+const arr = [
+    {function1(n) {
+        return n*2;
+    }},
+    {functio2(n) {
+        return n+1;
+    }}
+];
+
+function Weird(arr, n) {
+    let n_arr = [];
+    for (let i: number = 0; i < 2; i++) {
+        n_arr.push(arr[i](n));
+    }
+    return n_arr;
+}
+export{}
