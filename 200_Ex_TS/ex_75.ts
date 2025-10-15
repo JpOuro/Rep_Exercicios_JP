7960bd1b0b44717d2d194b501d9d88d81d6f6c24
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-10-15 09:45:51 -0300

    Escreva uma função de alta ordem que aceite um array de funções de callback e um valor, e retorne um novo array que contém os resultados de aplicar a função de callback ao valor, na ordem inversa.

---
+++ 200_Ex_TS/ex_75.ts
@@ -0,0 +1,19 @@
+let n: number = parseInt(prompt("Insira um valor inicial."));
+const arr = [
+    {function1(n) {
+        return n*2;
+    }},
+    {functio2(n) {
+        return n+1;
+    }}
+];
+
+function Weird(n, arr) {
+    let n_arr = [];
+    for (let i: number = arr.length - 1; i > -1; i--) {
+        n_arr.push(arr[i](n));
+    }
+    return n_arr;
+}
+Weird(n, arr);
+export{}
