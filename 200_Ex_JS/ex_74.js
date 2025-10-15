8da98f27972c6c3f3846dd3062586faa6b1fc9cd
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-10-15 09:06:09 -0300

    Escreva uma função de alta ordem que aceite uma função de callback e um array, e retorne um novo array que contém os resultados de aplicar a função de callback a cada elemento do array, sem alterar o array original

---
+++ 200_Ex_JS/ex_74.js
@@ -0,0 +1,12 @@
+const arr = [1, 2, 3];
+function Weird(arr, func) {
+    let n_arr = [];
+    for (let i = 0; i < n_arr.length; i++) {
+        n_arr = func(arr[i]);
+    }
+    return n_arr;
+}
+function doubler(n) {
+    return n*2;
+}
+Weird(arr, doubler);
