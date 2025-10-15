f45468d4c1c102e35db1ba0a3f7108e28f5fa166
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-10-15 08:41:11 -0300

    Escreva uma função que aceite um número, um array de funções de callback e um valor inicial, e retorne o resultado final após a aplicação de todas as funções de callback no valor inicial, o número de vezes especificado.

---
+++ 200_Ex_TS/ex_72.ts
@@ -0,0 +1,22 @@
+let n: number = parseInt(prompt("Insira um número de repetições."));
+let m: number = parseInt(prompt("Insira um valor inicial."));
+const arr = [
+    {function1(n) {
+        return n*2;
+    }},
+    {functio2(n) {
+        return n+1;
+    }}
+];
+
+function Weird(n, arr, m) {
+    let n_arr = [];
+    for (let i = 0; i < n_arr.length; i++) {
+        for (let j = 0; j < m; j++) {
+            n_arr.push(arr[i](n));
+        }
+    }
+    return n_arr;
+}
+Weird(n, arr, m);
+export{}
