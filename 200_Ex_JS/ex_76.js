2c73e1404fdafc4e7b43c3374896fd90a226ce0b
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-10-17 23:24:21 -0300

    Crie um objeto vazio e adicione três propriedades a ele, cada uma com diferentes tipos de valores.

---
+++ 200_Ex_JS/ex_76.js
@@ -0,0 +1,4 @@
+const Person = new Object();
+Person.name = "João";
+Person.age = 19;
+Person.Eye_c = "Brown";
---
+++ 200_Ex_TS/ex_76.ts
@@ -0,0 +1,9 @@
+interface Person {
+    
+}
+const eu: Person = {};
+
+(eu as any).name = "João";
+(eu as any).age = 19;
+(eu as any).Eye_c = "Brown";
+export{}
